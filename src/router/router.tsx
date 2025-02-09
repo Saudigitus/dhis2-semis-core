bc25ff5a7684e97dddb135665eaabfa2b4addd29
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-02-09 16:56:38 +0200

    celaning unecessary routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,26 @@
+import { HashRouter, Navigate, Route, Routes} from 'react-router-dom';
 import RouteList from './routeList';
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import WithSideBarLayout from '../layout/sidebarLayout/withSideBarLayout';
-import { EnrollmentsPage } from 'dhis2-semis-enrollment';
+// import { EnrollmentsPage } from 'dhis2-semis-enrollment';
 
 export default function Router() {
     return (
         <HashRouter>
-            {/* <Routes>
-                <Route path='/home' element={<div>kekeke</div>} />
-            </Routes> */}
-            <EnrollmentsPage />
+             <Routes>
+                <Route path='/' element={<Navigate to="semis" replace />} />
+                <Route path='/semis' element={<WithSideBarLayout />}>
+                    {
+                        RouteList().map((route, index) => (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={route.component}
+                            />
+                        ))
+                    }
+                </Route>
+            </Routes>
+            {/* <EnrollmentsPage/> */}
         </HashRouter>
     )
 }

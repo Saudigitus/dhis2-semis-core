1621d6f74bbb5ad1f428143e3f992eb511613126
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-02-12 02:52:13 +0200

    sidebar collapse/expand button dynamic position

--- src/layout/sidebarLayout/homeLayout.tsx
+++ src/layout/sidebarLayout/homeLayout.tsx
@@ -3,19 +3,19 @@ import { Outlet } from "react-router-dom"
 import { SideBar } from "dhis2-semis-components"
 import { useMenuData } from "../../hooks/menu/useMenuData"
 
-const WithSideBarLayout = () => {
+const HomeLayout = () => {
     const { menuData } = useMenuData()
 
     return (
         <div className={style.LayoutContainer}>
             <aside className={style.AsideContainer}>
-                <SideBar sideBarBtnPosition="top-offset" collapsed={false} sideBarData={menuData} />
+                <SideBar collapsed={false} sideBarData={menuData} />
             </aside>
-            <main className={style.MainContentContainer}>
+            <main className={style.HomeMainContentContainer}>
                 <Outlet />
             </main>
         </div>
     )
 }
 
-export default WithSideBarLayout
+export default HomeLayout
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,13 +2,14 @@ import RouteList from './routeList';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import WithSideBarLayout from '../layout/sidebarLayout/withSideBarLayout';
 import { EnrollmentPage } from 'dhis2-semis-enrollment';
+import HomeLayout from '../layout/sidebarLayout/homeLayout';
 
 export default function Router() {
     return (
         <HashRouter>
             <Routes>
                 <Route path='/' element={<Navigate to="/semis" replace />} />
-                <Route path='/semis' element={<WithSideBarLayout />}>
+                <Route path='/semis' element={<HomeLayout />}>
                     {
                         RouteList().map((route, index) => (
                             <Route
@@ -18,6 +19,8 @@ export default function Router() {
                             />
                         ))
                     }
+                </Route>
+                <Route path='/semis/enrollments' element={<WithSideBarLayout />}>
                     <Route path="/semis/enrollments" element={<EnrollmentPage />} />
                 </Route>
             </Routes>

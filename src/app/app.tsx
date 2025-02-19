a6b3a14e3399e6fa7d014d5a970d4345e8431c61
cnandza <cnandza@saudigitus.org>
2025-02-19 14:29:59 +0200

    chore: update dhis2-semis-enrollment to version 1.0.1-alpha.19 and wrap App component with RecoilRoot

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,17 @@
+import { RecoilRoot } from "recoil"
 import Router from "../router/router"
 import { HashRouter } from "react-router-dom"
 import { AppWrapper } from "dhis2-semis-components"
 
 const App = () => {
   return (
-    <AppWrapper dataStoreKey="semis/values">
-      <HashRouter>
-        <Router />
-      </HashRouter>
-    </AppWrapper>
+    <RecoilRoot>
+      <AppWrapper dataStoreKey="semis/values">
+        <HashRouter>
+          <Router />
+        </HashRouter>
+      </AppWrapper>
+    </RecoilRoot>
   )
 }
 

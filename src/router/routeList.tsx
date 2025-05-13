34af9015e80c471939924a82059c1aef202c0df0
cnandza <cnandza@saudigitus.org>
2025-05-13 09:48:58 +0200

    feat: update routing structure and add new routes for attendance and transfer

--- src/router/routeList.tsx
+++ src/router/routeList.tsx
@@ -1,10 +1,34 @@
 import Home from "../pages/home";
+import { EnrollmentPage } from "dhis2-semis-enrollment";
+import { App as Attendance } from "dhis2-semis-attendance"
+import { FinalResult } from "dhis2-semis-final-result"
+import { Transfer } from "dhis2-semis-transfer"
 
 export default function RouteList() {
     return [
         {
             path: "/semis/",
             component: <Home />
+        },
+        {
+            path: "/semis/enrollments",
+            component: <EnrollmentPage />
+        },
+        {
+            path: "/semis/attendance",
+            component: <Attendance />
+        },
+        {
+            path: "/semis/performance",
+            component: <></>
+        },
+        {
+            path: "/semis/transfer",
+            component: <Transfer />
+        },
+        {
+            path: "/semis/final-result",
+            component: <FinalResult />
         }
-    ]
+    ];
 }

fd8d52135ecc5b479b1be7d7e77c1736d714f43c
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-05-30 11:29:15 +0200

    refactor: update package name and version; adjust dependencies and remove unused routes
    - Changed package name from "semis-core-v2" to "semis-core" and updated version to "2.0.0-alpha".
    - Updated various dependencies to their latest versions, including @dhis2/app-runtime, @emotion/react, and @mui/material.
    - Removed unused imports and routes for Transfer, Reenrollment, and Performance in routeList.tsx.

--- src/router/routeList.tsx
+++ src/router/routeList.tsx
@@ -2,9 +2,9 @@ import Home from "../pages/home";
 import { EnrollmentPage } from "dhis2-semis-enrollment";
 import { App as Attendance } from "dhis2-semis-attendance"
 import { FinalResult } from "dhis2-semis-final-result"
-import { Transfer } from "dhis2-semis-transfer"
-import { Reenrollment } from "dhis2-semis-staff-reenroll"
-import { Performance } from "dhis2-semis-performance"
+// import { Transfer } from "dhis2-semis-transfer"
+// import { Reenrollment } from "dhis2-semis-staff-reenroll"
+// import { Performance } from "dhis2-semis-performance"
 
 export default function RouteList() {
     return [
@@ -20,21 +20,21 @@ export default function RouteList() {
             path: "/semis/attendance",
             component: <Attendance />
         },
-        {
-            path: "/semis/performance",
-            component: <Performance/>
-        },
-        {
-            path: "/semis/transfer",
-            component: <Transfer />
-        },
+        // {
+        //     path: "/semis/performance",
+        //     component: <Performance/>
+        // },
+        // {
+        //     path: "/semis/transfer",
+        //     component: <Transfer />
+        // },
         {
             path: "/semis/final-result",
             component: <FinalResult />
         },
-        {
-            path: "/semis/re-enroll",
-            component: <Reenrollment />
-        }
+        // {
+        //     path: "/semis/re-enroll",
+        //     component: <Reenrollment />
+        // }
     ];
 }

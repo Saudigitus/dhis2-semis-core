7a603a6e049e9553a4bbf6fe463b3f92c1f65088
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-05-30 16:42:24 +0200

    feat: update package.json and routeList.tsx to include new SEMIS modules
    - Added new dependencies: dhis2-semis-performance, dhis2-semis-staff-reenroll, and dhis2-semis-transfer-execute to package.json.
    - Updated routeList.tsx to include routes for Performance, Transfer, Transfer Execute, and Reenrollment components.

--- src/router/routeList.tsx
+++ src/router/routeList.tsx
@@ -2,9 +2,10 @@ import Home from "../pages/home";
 import { EnrollmentPage } from "dhis2-semis-enrollment";
 import { App as Attendance } from "dhis2-semis-attendance"
 import { FinalResult } from "dhis2-semis-final-result"
-// import { Transfer } from "dhis2-semis-transfer"
-// import { Reenrollment } from "dhis2-semis-staff-reenroll"
-// import { Performance } from "dhis2-semis-performance"
+import { Transfer } from "dhis2-semis-transfer"
+import { Reenrollment } from "dhis2-semis-staff-reenroll"
+import { Performance } from "dhis2-semis-performance"
+import { TransferExecute } from "dhis2-semis-transfer-execute"
 
 export default function RouteList() {
     return [
@@ -20,25 +21,25 @@ export default function RouteList() {
             path: "/semis/attendance",
             component: <Attendance />
         },
-        // {
-        //     path: "/semis/performance",
-        //     component: <Performance/>
-        // },
-        // {
-        //     path: "/semis/transfer",
-        //     component: <Transfer />
-        // },
-        // {
-        //     path: "/semis/transfer-execute",
-        //     component: <TransferExecute />
-        // },
+        {
+            path: "/semis/performance",
+            component: <Performance/>
+        },
+        {
+            path: "/semis/transfer",
+            component: <Transfer />
+        },
+        {
+            path: "/semis/transfer-execute",
+            component: <TransferExecute />
+        },
         {
             path: "/semis/final-result",
             component: <FinalResult />
         },
-        // {
-        //     path: "/semis/re-enroll",
-        //     component: <Reenrollment />
-        // }
+        {
+            path: "/semis/re-enroll",
+            component: <Reenrollment />
+        }
     ];
 }

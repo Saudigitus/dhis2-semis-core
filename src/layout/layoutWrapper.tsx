e14a5b079b934df2fedddf29dbdbe9582496f213
EdsonNhancale <edsonnhancale@gmail.com>
2025-09-12 11:28:07 +0200

    fix: solved issue when open the app

--- src/layout/layoutWrapper.tsx
+++ src/layout/layoutWrapper.tsx
@@ -5,18 +5,8 @@ import {  WithPadding } from "dhis2-semis-components"
 
 const LayoutWrapper = ({ children }: { children: ReactElement }) => {
     const { pathname } = useLocation()
-    // const { defaults } = useSchoolCalendar()
-    const { add, urlParameters } = useUrlParams()
-    const { academicYear, sectionType } = urlParameters()
-
-
-    useEffect(() => {
-        if (pathname !== "/semis" && sectionType && (!academicYear || academicYear.trim() === "")) {
-            setTimeout(() => {
-                add("academicYear", defaults?.academicYear)
-            }, 100)
-        }
-    }, [pathname])
+    const { urlParameters } = useUrlParams()
+    const { sectionType } = urlParameters()
 
     if (!sectionType && (pathname !== "/semis" && pathname !== "/semis/configuration" && !pathname.includes('/semis/school-calendar'))) {
         return (

0216a6acfb24042771d311b8f67fd8b7a2268655
EdsonNhancale <edsonnhancale@gmail.com>
2025-09-12 11:16:53 +0200

    feat: hided school calendar if not exist semis configurations

--- src/layout/layoutWrapper.tsx
+++ src/layout/layoutWrapper.tsx
@@ -1,22 +1,12 @@
 import { useLocation } from "react-router-dom"
 import { ReactElement } from "react"
 import { useUrlParams } from "dhis2-semis-functions"
-import {  WithPadding } from "dhis2-semis-components"
+import { WithPadding } from "dhis2-semis-components"
 
 const LayoutWrapper = ({ children }: { children: ReactElement }) => {
     const { pathname } = useLocation()
-    // const { defaults } = useSchoolCalendar()
-    const { add, urlParameters } = useUrlParams()
-    const { academicYear, sectionType } = urlParameters()
-
-
-    useEffect(() => {
-        if (pathname !== "/semis" && sectionType && (!academicYear || academicYear.trim() === "")) {
-            setTimeout(() => {
-                add("academicYear", defaults?.academicYear)
-            }, 100)
-        }
-    }, [pathname])
+    const { urlParameters } = useUrlParams()
+    const { sectionType } = urlParameters()
 
     if (!sectionType && (pathname !== "/semis" && pathname !== "/semis/configuration" && !pathname.includes('/semis/school-calendar'))) {
         return (

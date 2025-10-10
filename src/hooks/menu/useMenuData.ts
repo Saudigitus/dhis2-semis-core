b61deb6f8d0fdca5e2ea52e95efc3e0b7423c5cf
EdsonNhancale <edsonnhancale@gmail.com>
2025-10-10 14:44:36 +0200

    chore: updating configuration module

--- src/hooks/menu/useMenuData.ts
+++ src/hooks/menu/useMenuData.ts
@@ -68,7 +68,7 @@ const useMenuData = () => {
       copy[indexConfigurations].displayInMenu = false;
       copy[indexConfigurations].subItems = [];
     } else {
-      // If no academic year is defined, remove "SEMIS-Calendar"
+      // If no academic year defined, remove "SEMIS-Calendar"
       if (!schoolCalendar?.academicYear) {
         copy[indexConfigurations].subItems = copy[indexConfigurations].subItems.filter(
           (subItem: any) => subItem.id != "school-calendar"

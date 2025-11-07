b2884954a75d01caf4fecc2aba28e5dbd1772184
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-11-07 15:20:33 +0200

    chore: Update dhis2-semis-components to 2.0.1 and dhis2-semis-functions to 0.0.7-alpha.58

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,9 +3,31 @@ import { useConfig } from "@dhis2/app-runtime"
 import { AppWrapper } from "dhis2-semis-components"
 import InitializeWrapper from "../components/wrapper/InitializeWrapper"
 import i18n from "../locales"
+import { useEffect, useState } from "react"
+import { useCacheData } from "dhis2-semis-functions"
 
 const App = () => {
   const { baseUrl } = useConfig()
+  const { initializeDB } = useCacheData();
+  const [loading, setLoading] = useState(true); // Start with loading true
+
+  useEffect(() => {
+    const initAndSave = async () => {
+      try {
+        await initializeDB(); // Wait for DB to be fully ready
+        console.log('Data loaded successfully');
+      } catch (error) {
+        console.error('DB error:', error);
+      } finally {
+        setLoading(false); // Set loading to false after operations complete
+      }
+    };
+    initAndSave();
+  }, []); // Run only once on mount
+
+  if (loading) {
+    return <span>{('Loading...')}</span>;
+  }
 
   return (
     <>

1aa47de128ed04df4ace8cc9c904e69ad9a78177
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-12-12 16:41:47 +0200

    %%npm_config_msg%%

--- src/apps/enrollment/src/app/App.tsx
+++ src/apps/enrollment/src/app/App.tsx
@@ -8,7 +8,7 @@ import { D2I18n } from 'dhis2-semis-types'
 
 const Enrollment = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()
-    //This is test 3.0
+    //This is test 4.0
 
     return (
         <Router i18n={i18n as unknown as any} />

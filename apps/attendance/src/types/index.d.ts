d4dbac079c6dcc035fcd45a4c935830c910ff7ac
EdsonNhancale <edsonnhancale97@gmail.com>
2025-12-12 11:55:12 +0200

    chore: remove attendance app files and dependencies

--- apps/attendance/src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@dhis2/d2-i18n";
-declare module "@dhis2/ui";
-declare module "*.module.css" {
-    const styles: { [key: string]: string };
-    export default styles;
-}
-declare module "*.css";
-declare module "*.js";

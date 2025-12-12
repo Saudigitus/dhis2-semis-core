f0900d6cd458dbee4a44e1864b095728796e91e8
EdsonNhancale <edsonnhancale97@gmail.com>
2025-12-12 11:56:06 +0200

    Squashed 'src/apps/enrollment/' content from commit 387677f
    git-subtree-dir: src/apps/enrollment
    git-subtree-split: 387677f24f46353299e0764dcb0b15fc5a8c5d27

---
+++ src/components/modal/saveEnrollment/ModalManager.tsx
@@ -0,0 +1,147 @@
+import { format } from "date-fns";
+import { useRecoilState } from "recoil";
+import ModalContent from "./ModalContent";
+import React, { useEffect, useState } from "react";
+import { TableDataRefetch } from "dhis2-semis-types"
+import { ModalManagerInterface } from "../../../types/modal/ModalProps";
+import useGetSelectedKeys from "../../../hooks/config/useGetSelectedKeys";
+import { ModalComponent, useGetUsedProgramStages, } from "dhis2-semis-components";
+import { enrollmentPostBody, enrollmentUpdateBody } from "../../../utils/enrollment";
+import useGetEnrollmentUpdateInitialValues from "../../../hooks/form/useGetEnrollmentUpdateInitialValues";
+import { useGetAttributes, useGetPatternCode, useSaveTei, useUrlParams, useGetSectionTypeLabel, RulesEngine, capitalizeString } from "dhis2-semis-functions";
+
+
+function ModalManager(props: ModalManagerInterface) {
+    const { urlParameters, useQuery } = useUrlParams();
+    const { school, schoolName } = urlParameters;
+    const { saveTei, loading: saving } = useSaveTei();
+    const { sectionName } = useGetSectionTypeLabel();
+    const enrollment = useQuery.get("enrollment") as string
+    const [refetch, setRefetch] = useRecoilState(TableDataRefetch);
+    const trackedEntity = useQuery.get("trackedEntity") as string
+    const { program: programData, dataStoreData } = useGetSelectedKeys()
+    const { attributes = [] } = useGetAttributes({ programData: programData! });
+    const programStagesToSave = useGetUsedProgramStages({ sectionType: sectionName });
+    const { returnPattern, loadingCodes, generatedVariables } = useGetPatternCode();
+    const { open, setOpen, saveMode, initialValues: initialValuesFromSearch, formFields = [], formVariablesFields, setFormInitialValues, i18n } = props;
+    const { getInitialValues, initialValues: updateInitialValues, loading: initialValuesLoading, enrollmentEvents } = useGetEnrollmentUpdateInitialValues()
+
+    let allInitialValues = {
+        orgUnit: school,
+        registerschoolstaticform: schoolName,
+        enrollment_date: format(new Date(), "yyyy-MM-dd"),
+    }
+
+    const [values, setValues] = useState<{ [key: string]: any }>({ ...allInitialValues });
+
+    const { runRulesEngine, updatedVariables } = RulesEngine({
+        values: values,
+        variables: formFields,
+        program: programData!.id,
+        type: "programStageSection",
+    })
+
+
+    useEffect(() => {
+        runRulesEngine({ overrideVariables: formFields, overrideValues: values })
+    }, [values])
+
+    useEffect(() => {
+        if (open && saveMode == "CREATE")
+            void returnPattern(attributes);
+
+        if (open && saveMode == "UPDATE")
+            void getInitialValues(trackedEntity, enrollment);
+    }, [open]);
+
+    useEffect(() => {
+        return () => {
+            if (!open)
+                setValues({});
+            setFormInitialValues && setFormInitialValues({})
+        }
+    }, [open])
+
+    const handleCloseModal = () => setOpen(false);
+
+    const handleChange = (e: { field: any; value: string; name: string }) => {
+        // const { name, value } = e;
+        // setValues(prev => ({
+        //     ...allInitialValues,
+        //     ...prev,
+        //     [name]: value,
+        // }));
+    };
+
+
+    function onSubmit(e: Record<string, any>): void {
+        const data = () => {
+            if (saveMode === "CREATE") {
+                return enrollmentPostBody({
+                    values: e,
+                    orgUnitId: school!,
+                    programStagesToSave,
+                    programId: programData?.id!,
+                    formVariablesFields: formVariablesFields,
+                    enrollmentDate: e?.enrollment_date,
+                    trackedEntityType: programData?.trackedEntityType?.id!,
+                    trackedEntityId: initialValuesFromSearch!["trackedEntity"]
+                });
+            }
+
+            if (saveMode === "UPDATE") {
+                return enrollmentUpdateBody({
+                    formVariablesFields: formVariablesFields,
+                    enrollmentId: e?.enrollment,
+                    enrollmentDate: e?.enrollment_date,
+                    trackedEntityId: e?.trackedEntity,
+                    trackedEntityType: programData?.trackedEntityType?.id!,
+                    orgUnitId: school!,
+                    programId: programData?.id!,
+                    formValues: e,
+                    events: enrollmentEvents?.events,
+                });
+            }
+        };
+
+        saveTei({
+            data: data(),
+            messages: {
+                error: `${i18n.t("Could not conclude the opertation.")}`,
+                sucess: `${i18n.t("Operation concluded successfully")}`,
+            },
+            handleComplete: () => { handleCloseModal(); setRefetch(!refetch) },
+        });
+    }
+
+    return (
+        <ModalComponent
+            open={open}
+            handleClose={handleCloseModal}
+            loading={loadingCodes || initialValuesLoading}
+            title={i18n.t('Single {{section}} Enrollment {{mode}}', {
+                section: i18n.t(sectionName),
+                mode: saveMode === 'UPDATE' ? i18n.t('Update') : ''
+            })}
+        >
+            <ModalContent
+                loading={saving!}
+                onSubmit={onSubmit}
+                formValues={values}
+                onChange={handleChange}
+                setFormValues={setValues}
+                onCancel={handleCloseModal}
+                formFields={updatedVariables}
+                trackedEntity={trackedEntity}
+                initialValues={{
+                    ...allInitialValues,
+                    ...generatedVariables,
+                    ...updateInitialValues,
+                    ...initialValuesFromSearch,
+                }}
+            />
+        </ModalComponent>
+    );
+}
+
+export default ModalManager;

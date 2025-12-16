e89f27bf488d79750c29645d32dd56d2192f0e98
CelsoDeCarvalho <celsomomade361@gmail.com>
2025-12-16 15:06:36 +0200

    Squashed 'src/modules/transfer/' content from commit b91e48e
    git-subtree-dir: src/modules/transfer
    git-subtree-split: b91e48ec3c6d3101b614cabb1e220a78d586c017

---
+++ src/components/modal/modalTransfer.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import style from './modalTransfer.module.css'
+import { ModalComponent, WithPadding } from "dhis2-semis-components";
+import { ApproveTranferProps } from "../../types/modal/ModalProps";
+import { useGetSectionTypeLabel, useUrlParams } from "dhis2-semis-functions";
+import { useTransferTEI } from "../../hooks/tei/useTransfer";
+import useGetSelectedKeys from "../../hooks/config/useGetSelectedKeys";
+
+function ApproveTranfer(props: ApproveTranferProps): React.ReactElement {
+    const { modalDetails, setModalDetails, i18n } = props;
+    const { urlParameters } = useUrlParams()
+    const { school, schoolName } = urlParameters
+    const { sectionName } = useGetSectionTypeLabel();
+    const { program, dataStoreData } = useGetSelectedKeys()
+    const trackedEntityAttributes = program!?.trackedEntityType?.trackedEntityTypeAttributes
+    const programTrackedEntityAttributes = program!?.programTrackedEntityAttributes
+    const { loading, transferTEI, rejectTEI, loadingEvents } = useTransferTEI({ selectedTei: modalDetails.row, handleCloseApproval: () => setModalDetails({ open: false }) });
+
+    const actions = [
+        { id: "cancel", name: i18n.t("Cancel"), disabled: false, onClick: () => setModalDetails({ open: false }) },
+        {
+            id: "confirm", name: i18n.t("Confirm"), primary: true, loading: !!(loadingEvents || loading), disabled: !!(loadingEvents || loading), onClick: () => {
+                if (modalDetails?.approved) {
+                    transferTEI(school)
+                } else {
+                    rejectTEI()
+                }
+            }
+        },
+    ];
+
+    return (
+        <ModalComponent
+            children={
+                <WithPadding p="10px 0px">
+                    {modalDetails?.approved
+                        ? <span>
+                            <span className="text-danger">
+                                {i18n.t("Attention")}:
+                            </span>
+                            {
+                                i18n.t('This action will transfer this {{section}} into this school.', {
+                                    section: `${i18n.t(sectionName)}s`,
+                                })
+                            }
+                        </span>
+                        : <span>
+                            <span className="text-danger">
+                                {i18n.t("Attention")}:
+                            </span>
+                            {
+                                i18n.t('You are about to reject the {{section}} transfer to this school.', {
+                                    section: `${i18n.t(sectionName)}s`,
+                                })
+                            }
+                        </span>
+                    }
+
+                    <div className={style.divider}></div>
+
+                    {modalDetails?.approved
+                        ? <div className="py-2">
+                            {i18n.t("Are you sure you want to approve the transfer of ")} <strong>{modalDetails?.row?.[trackedEntityAttributes[1]?.trackedEntityAttribute.id ?? programTrackedEntityAttributes[2]?.trackedEntityAttribute.id]} {modalDetails?.row?.[trackedEntityAttributes[0]?.trackedEntityAttribute?.id ?? programTrackedEntityAttributes[3]?.trackedEntityAttribute.id]} </strong>{" "} {i18n.t("from")}{" "}
+                            <strong>{modalDetails?.row?.[dataStoreData?.transfer?.originSchool]}</strong>{" "} {i18n.t("to")} {" "}
+                            <strong>{schoolName}</strong>?
+                        </div>
+                        : <div className="py-2">
+                            {i18n.t("Are you sure you want to reject the transfer of")}  <strong>{modalDetails?.row?.[trackedEntityAttributes[1]?.trackedEntityAttribute.id ?? programTrackedEntityAttributes[2]?.trackedEntityAttribute.id]} {modalDetails?.row?.[trackedEntityAttributes[0]?.trackedEntityAttribute?.id ?? programTrackedEntityAttributes[3]?.trackedEntityAttribute.id]}</strong>{" "} {i18n.t("from")}{" "}
+                            <strong>{modalDetails?.row?.[dataStoreData?.transfer?.originSchool]}</strong>{" "} {i18n.t("to")} {" "}
+                            <strong>{schoolName}</strong>
+                        </div>
+                    }
+                </WithPadding>
+            }
+            open={modalDetails.open}
+            handleClose={() => setModalDetails({ open: false })}
+            title={i18n.t("Transfer Approval")}
+            showActions
+            size="large"
+            actions={actions}
+        />
+    );
+}
+
+export default ApproveTranfer;

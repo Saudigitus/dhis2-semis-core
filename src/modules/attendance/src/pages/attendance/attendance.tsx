99fed461748c9bc9461f9c84edb0e4d56b1a8788
EdsonNhancale <edsonnhancale97@gmail.com>
2025-12-17 09:34:56 +0200

    style: fix whitespace

--- src/modules/attendance/src/pages/attendance/attendance.tsx
+++ src/modules/attendance/src/pages/attendance/attendance.tsx
@@ -84,6 +84,7 @@ export default function Attendance({ i18n }: { i18n: D2I18n }) {
 
     return (
         <div style={{ height: "85vh" }}>
+            Edson Nhancale
             {
                 !(Boolean(schoolName) && Boolean(school) && areAllSelected()) ?
                     <InfoPageHolder i18n={i18n} />

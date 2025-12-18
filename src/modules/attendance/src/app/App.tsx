424119d0bd6eee138cfb6ea135f1f6112079b5c2
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2025-12-18 10:41:02 +0200

    feat: update attendance and enrollment apps

--- src/modules/attendance/src/app/App.tsx
+++ src/modules/attendance/src/app/App.tsx
@@ -9,7 +9,9 @@ import translation from '../locales/index'
 const App = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()
     const language = i18n == undefined ? translation : i18n
-  //just do it
+  //just do it again 
+  //and again
+  
     return (
         // <AppWrapper
         //     i18n={language}
--- src/modules/enrollment/src/app/App.tsx
+++ src/modules/enrollment/src/app/App.tsx
@@ -5,7 +5,7 @@ import { useConfig } from '@dhis2/app-runtime'
 import { HashRouter } from 'react-router-dom'
 import { AppWrapper } from 'dhis2-semis-components'
 import { D2I18n } from 'dhis2-semis-types'
-// import translation from '../locales/index'
+// import translation from '../locales/index' what about here
 
 const Enrollment = ({ i18n }: { i18n: D2I18n }) => {
     const { baseUrl } = useConfig()

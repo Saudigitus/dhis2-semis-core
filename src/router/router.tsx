d1b70ad53768b09559721a3b48d115f60c56eaf9
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2026-02-16 12:11:06 +0200

    feat: add global shell URL sync and update translations
    Add SyncUrlWithGlobalShell component to dispatch popstate events for compatibility with DHIS2 Global Shell when using react-router v6+. Update proxy configuration in package.json for development. Extend translation files (en, fr, pt) with new configuration error messages and correct existing entries. Update i18n template with new keys and fix a typo in the English translation.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,19 +1,32 @@
 import RouteList from './routeList';
 import WithSideBarLayout from '../layout/withSideBarLayout';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { SyncUrlWithGlobalShell } from './syncUrlWithGlobalShell';
 
 export default function Router() {
+
     return (
         <HashRouter>
             <Routes>
                 <Route path='/' element={<Navigate to="/semis" replace />} />
-                <Route path='/semis' element={<WithSideBarLayout />}>
+                <Route path='/semis' element={
+                    <>
+                        <SyncUrlWithGlobalShell />
+                        <WithSideBarLayout />
+                    </>
+                }
+                >
                     {
                         RouteList().map((route, index) => (
                             <Route
                                 key={index}
                                 path={route.path}
-                                element={route.component}
+                                element={
+                                    <>
+                                        <SyncUrlWithGlobalShell />
+                                        {route.component}
+                                    </>
+                                }
                             />
                         ))
                     }

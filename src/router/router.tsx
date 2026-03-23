d4fa384c7b7084ed204371abc88c5d7a05d490f1
Edson Nhancale <edsonNhancale97@gmail.com>
2026-03-23 16:59:15 +0200

    chore: update proxy URL and add react-query dependency
    - Change development proxy from emis.im.dhis2.org/dev42 to dev-dhis2.mineduc.gov.rw/dev
    - Add @tanstack/react-query package for state management
    - Configure query client with 5-hour stale time and disabled window focus refetch
    - Update i18n translations for error/warning details and tracker type summary
    - Remove unused manifest translation entries

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,36 +2,48 @@ import RouteList from './routeList';
 import WithSideBarLayout from '../layout/withSideBarLayout';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { SyncUrlWithGlobalShell } from './syncUrlWithGlobalShell';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 5 * 60 * 60 * 1000, // 5 hours
+        },
+    },
+});
 
 export default function Router() {
 
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/' element={<Navigate to="/semis" replace />} />
-                <Route path='/semis' element={
-                    <>
-                        <SyncUrlWithGlobalShell />
-                        <WithSideBarLayout />
-                    </>
-                }
-                >
-                    {
-                        RouteList().map((route, index) => (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <>
-                                        <SyncUrlWithGlobalShell />
-                                        {route.component}
-                                    </>
-                                }
-                            />
-                        ))
+        <QueryClientProvider client={queryClient}>
+            <HashRouter>
+                <Routes>
+                    <Route path='/' element={<Navigate to="/semis" replace />} />
+                    <Route path='/semis' element={
+                        <>
+                            <SyncUrlWithGlobalShell />
+                            <WithSideBarLayout />
+                        </>
                     }
-                </Route>
-            </Routes>
-        </HashRouter>
+                    >
+                        {
+                            RouteList().map((route, index) => (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <>
+                                            <SyncUrlWithGlobalShell />
+                                            {route.component}
+                                        </>
+                                    }
+                                />
+                            ))
+                        }
+                    </Route>
+                </Routes>
+            </HashRouter>
+        </QueryClientProvider>
     );
 }
